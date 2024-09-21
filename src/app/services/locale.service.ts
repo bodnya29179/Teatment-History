9f60958e882682eddb9377826896744c31ef5705
Yevhen Bodnia <bodnya29179@gmail.com>
2024-09-22 01:24:30 +0300

    Refactor

---
+++ src/app/services/locale.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Language } from '../models';
+
+const LOCALE_FIELD = 'locale';
+
+@Injectable()
+export class LocaleService {
+  constructor(private readonly translate: TranslateService) {}
+
+  get currentLanguage(): Language | undefined {
+    return (this.translate.currentLang || this.translate.defaultLang || this.localeLanguage) as Language;
+  }
+
+  get localeLanguage(): Language | undefined {
+    return localStorage.getItem(LOCALE_FIELD) as Language;
+  }
+
+  setLanguage(language: Language): void {
+    this.translate.setDefaultLang(language);
+    this.translate.use(language);
+    localStorage.setItem(LOCALE_FIELD, language);
+  }
+}
