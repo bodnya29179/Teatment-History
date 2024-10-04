40cf4d58bbb82a76fbc482f6a2caccfd32f440f9
Yevhen Bodnia <bodnya29179@gmail.com>
2024-10-04 21:41:16 +0300

    Refactor main.js

--- main.js
+++ main.js
@@ -3,14 +3,48 @@ const { spawn } = require('child_process');
 const path = require('path');
 const url = require('url');
 
+const PLATFORMS = Object.freeze({
+  macos: 'darwin',
+  windows: 'win32',
+  linux: 'linux',
+});
+
+const APP_EVENTS = Object.freeze({
+  ready: 'ready',
+  activate: 'activate',
+  close: 'window-all-closed',
+});
+
+const CLIENT_BUILD_ROOT_PATH = 'dist/treatment-history/browser';
+
+const IS_DEV = !app.isPackaged;
+
 let serverProcess = null;
 
-function isDev() {
-  return !app.isPackaged;
-}
+app.disableHardwareAcceleration();
+
+app.on(APP_EVENTS.ready, () => {
+  runServer();
+  createWindow();
+});
+
+app.on(APP_EVENTS.activate, () => {
+  if (!BrowserWindow.getAllWindows().length) {
+    createWindow();
+  }
+});
+
+app.on(APP_EVENTS.close, () => {
+  if (process.platform !== PLATFORMS.macos) {
+    app.quit();
+  }
+
+  stopServer();
+});
 
 function createWindow() {
   const win = new BrowserWindow({
+    icon: path.join(__dirname, CLIENT_BUILD_ROOT_PATH, 'assets/icons/favicon.png'),
     width: 940,
     minWidth: 940,
     height: 680,
@@ -24,48 +58,21 @@ function createWindow() {
 
   win.loadURL(
     url.format({
-      pathname: path.join(__dirname, 'dist/treatment-history/browser/index.html'),
+      pathname: path.join(__dirname, CLIENT_BUILD_ROOT_PATH, 'index.html'),
       protocol: 'file:',
       slashes: true,
     })
   );
 }
 
-app.disableHardwareAcceleration();
+function runServer() {
+  serverProcess = spawn(
+    'node',
+    [path.join(__dirname, 'server/server.js')],
+    { stdio: 'inherit', shell: true },
+  );
+}
 
-app.on('ready', () => {
-  if (isDev()) {
-    serverProcess = spawn('node', [path.join(__dirname, 'server/server.js')], {
-      stdio: 'inherit',
-      shell: true,
-    });
-
-    // Optional: Log server output to the console
-    serverProcess.stdout.on('data', (data) => {
-      console.log(`Server output: ${data}`);
-    });
-
-    serverProcess.stderr.on('data', (data) => {
-      console.error(`Server error: ${data}`);
-    });
-  }
-
-  createWindow();
-});
-
-app.on('activate', () => {
-  if (!BrowserWindow.getAllWindows().length) {
-    createWindow();
-  }
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-
-  /* Stop the Node.js server when Electron closes */
-  if (serverProcess) {
-    serverProcess.kill();
-  }
-});
+function stopServer() {
+  serverProcess?.kill();
+}
